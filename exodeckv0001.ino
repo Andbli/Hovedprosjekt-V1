672cde22b8159010a00d3a035bd38b0711c28fdb
Andreas <111028040+Andbli@users.noreply.github.com>
2025-02-11 21:18:22 +0100

    V1

---
+++ exodeckv0001.ino
@@ -0,0 +1,76 @@
+#include <BleKeyboard.h>
+BleKeyboard bleKeyboard("ExoDeck V1", "AndreasBlindheim", 100); //Name, Manufacturer, Battery
+
+//Pins
+const int button1 = 2;
+const int button2; //= 1;
+const int button3; //= 2;
+const int button4; //= 3;
+
+const int rotary1; //= 4;
+const int rotary2; //= 5;
+const int rotary3; //= 6;
+const int rotary4; //= 7;
+
+
+
+//Variables
+
+int b1val = 0; //Variable that stores current state of button.
+int b2val = 0;
+int b3val = 0;
+int b4val = 0;
+
+unsigned long b1last; //Variable that stores the last time the button was pressed with internal clock.
+unsigned long b2last;
+unsigned long b3last;
+unsigned long b4last;
+
+
+const int buttonpressdebouce = 50; //The time it should be since last button press in milliseconds
+
+
+
+
+void setup() {
+  bleKeyboard.begin(); //Starts the bluetooth keyboard
+
+  //pinmode
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
+
+  pinMode(rotary1, INPUT);
+  pinMode(rotary2, INPUT);
+  pinMode(rotary3, INPUT);
+  pinMode(rotary4, INPUT);
+
+
+}
+
+void loop() {
+  
+  if(bleKeyboard.isConnected()) {
+
+    //Read all componenents
+    b1val = digitalRead(button1);
+    b2val = digitalRead(button2);
+    b3val = digitalRead(button3);
+    b4val = digitalRead(button4);
+
+    if(b1val == 1 && millis() - b1last > buttonpressdebouce){ //Condition that check if the button is currectly pressed and that millis() - b1last is more than buttonpressdebounce.
+      
+      bleKeyboard.press(KEY_LEFT_GUI);
+      bleKeyboard.press('L');
+      delay(50);
+      bleKeyboard.releaseAll();
+      buttonpressdebouce = millis();
+    } 
+
+
+
+
+  }
+
+}
