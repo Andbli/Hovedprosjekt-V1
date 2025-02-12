d0a0250204babb02ce82abbd831493cb5c33dae2
Andreas <111028040+Andbli@users.noreply.github.com>
2025-02-12 18:20:46 +0100

    Update exodeckv0001.ino
    I commented out the unused values and switched to an internal pull-up resistor (don’t forget to connect the buttons to GND!). The code is now functional and can be compiled. I encountered an error that necessitated switching to version 2.0.11 for the ESP32 in the board manager.

--- exodeckv0001.ino
+++ exodeckv0001.ino
@@ -2,15 +2,15 @@
 BleKeyboard bleKeyboard("ExoDeck V1", "AndreasBlindheim", 100); //Name, Manufacturer, Battery
 
 //Pins
-const int b1 = 2;
-const int b2; //= 1;
-const int b3; //= 2;
-const int b4; //= 3;
+const int b1 = 18;
+//const int b2 = 1;
+//const int b3 = 2;
+//const int b4 = 3;
 
-const int rotary1; //= 4;
-const int rotary2; //= 5;
-const int rotary3; //= 6;
-const int rotary4; //= 7;
+//const int rotary1 = 4;
+//const int rotary2 = 5;
+//const int rotary3 = 6;
+//const int rotary4 = 7;
 
 
 
@@ -34,17 +34,18 @@ const int buttonpressdebouce = 50; //The time it should be since last button pre
 
 void setup() {
   bleKeyboard.begin(); //Starts the bluetooth keyboard
+  delay(500);
 
   //pinmode
-  pinMode(b1, INPUT);
-  pinMode(b2, INPUT);
-  pinMode(b3, INPUT);
-  pinMode(b4, INPUT);
+  pinMode(b1, INPUT_PULLUP);
+//  pinMode(b2, INPUT);
+//  pinMode(b3, INPUT);
+//  pinMode(b4, INPUT);
 
-  pinMode(rotary1, INPUT);
-  pinMode(rotary2, INPUT);
-  pinMode(rotary3, INPUT);
-  pinMode(rotary4, INPUT);
+//  pinMode(rotary1, INPUT);
+//  pinMode(rotary2, INPUT);
+//  pinMode(rotary3, INPUT);
+//  pinMode(rotary4, INPUT);
 
 
 }
@@ -55,17 +56,17 @@ void loop() {
 
     //Read all componenents
     b1val = digitalRead(b1);
-    b2val = digitalRead(b2);
-    b3val = digitalRead(b3);
-    b4val = digitalRead(b4);
+    //b2val = digitalRead(b2);
+    //b3val = digitalRead(b3);
+    //b4val = digitalRead(b4);
 
-    if(b1val == 1 && millis() - b1last > buttonpressdebouce){ //Condition that check if the button is currectly pressed and that millis() - b1last is more than buttonpressdebounce.
+    if(b1val == LOW && millis() - b1last > buttonpressdebouce){ //Condition that check if the button is currectly pressed and that millis() - b1last is more than buttonpressdebounce.
       
       bleKeyboard.press(KEY_LEFT_GUI);
-      bleKeyboard.press('L');
+      bleKeyboard.press('l');
       delay(50);
       bleKeyboard.releaseAll();
-      buttonpressdebouce = millis();
+      b1last = millis();
     } 
 
 
